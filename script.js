6e00eec98da90f539478d758586019eea71125dd
Xeni <shkelzen.dunisha@umsh.edu.al>
2022-04-11 22:49:52 -0400

    Needs more work, will return once I am better. It is fully functional though

--- script.js
+++ script.js
@@ -8,7 +8,19 @@ const subs = (a, b) => a - b;
 const mult = (a, b) => a * b;
 
 //division
-const divs = (a, b) => a / b;
+const divs = (a, b) => b == 0 ? 'Don\'t break my calculator!' : a / b; 
+
+//percentage
+const perc = (operand, a, b) =>  {
+    let tmp = (a * b)/100;
+    if(operand == '+') {
+        return a += tmp;
+    } else if (operand == '-') {
+        return a -= tmp;
+    } else {
+    return;
+    }
+};
 
 // switch between operands
 const operate = (operator, num1, num2) => {
@@ -25,43 +37,76 @@ const operate = (operator, num1, num2) => {
         case "/":
             return divs(num1, num2);
             break;
-            default:
-                return "Game over";
+        default:
+            return "Game over";
     }
 };
 
-console.log("this is operacioni " + operate('+', 6, 7));
 
 //grab screen and display numbers clicked
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
-console.log(buttons);
 let num1 = '';
 let num2 = '';
 let operand = '';
-buttons.forEach(button => button.addEventListener('click', (e) => {
+
+// function doing everything
+const calculate = (e) => {
     let clicked = e.target.firstChild.textContent;
-    let clickedToNum = Number(clicked);
-    let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    let opr = ['+', '-', '/', '%', '*'];
-    console.log(typeof clickedToNum)
+    let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let opr = ['+', '-', '/', '*'];
+    let total = '';
 
-    return (numbers.indexOf(clicked) != -1) && (operand == '') ? (num1 += clicked,display.textContent = `${num1} ${operand} ${num2}`) : opr.indexOf(clicked) != -1 ? (operand = clicked,display.textContent = `${num1} ${operand} ${num2}`) : num1 != '' && operand != '' && (numbers.indexOf(clicked) != -1)? (num2 += clicked,display.textContent = `${num1} ${operand} ${num2}`) : console.log((operate(operand, Number(num1), Number(num2))), display.textContent = `${operate(operand, Number(num1), Number(num2))}`);
-                
+    if(display.textContent.length <= 22) {
+        if(clicked == 'AC') {
+            display.textContent = '';
+            num1 = '';
+            num2 = '';
+            operand = '';
+        } else if(clicked == 'C') {
+            if(num1 != '' && operand == '') {
+                num1 = num1.split('').splice(0, num1.length -1).join('');
+                display.textContent = `${num1}`;
+            } else if( operand != '') {
+                num2 = num2.split('').splice(0, num2.length -1).join('');
+                display.textContent = `${num1} ${operand} ${num2}`;        }
+        } else if(clicked == '.' && num2 != '' && num1 != '' && num2.indexOf('.') == -1) {
+            num2 += '.';
+            display.textContent = `${num1} ${operand} ${num2}`;
+        } else if(clicked == '.' && num1 != '' && num1.indexOf('.') == -1) {
+            num1 += '.';
+            display.textContent = `${num1} ${operand} ${num2}`;
+        } else if(numbers.indexOf(clicked) != -1 && operand == '') {
+            num1 += clicked; 
+            display.textContent = `${num1} ${operand} ${num2}`;
+        } else if(opr.indexOf(clicked) != -1 && num1 != '') {
+            operand = clicked;
+            display.textContent = `${num1} ${operand} ${num2}`;
+        } else if(num1 != '' && operand != '' && (numbers.indexOf(clicked) != -1)) {
+            num2 += clicked;
+            display.textContent = `${num1} ${operand} ${num2}`;
+        } else if(num1 != '' && num2 != '' && operand != ''){
+            if(clicked == '%') {
+            let temp = perc(operand, Number(num1), Number(num2));
+            total = total + temp;
+            display.textContent = `${total}`;
+            num1 = temp;
+            num2 = '';
+            operand = '';
+            } else if(opr.indexOf(clicked) != -1 || clicked == '=') {
+            let temp = operate(operand, Number(num1), Number(num2));
+            total = total + temp;
+            display.textContent = `${total}`;
+            num1 = temp;
+            num2 = '';
+            operand = '';
+            } 
+        }
+    }
+};
 
-    // if(display.textContent.length < 30) {
-    //     if((opr.indexOf(clicked) != -1) && (num1 != '')) {
-    //         console.log(clicked);
-    //         operand = clicked;
-    //         display.textContent = operand;
-    //     }
-    //     else if(clickedToNum != NaN) {
-    //         // console.log(clicked)
-    //             num1 += clickedToNum};
-    //             display.textContent = num1;
-
-    //     }
-}))
+//grab the buttons and place a for-each loop
+buttons.forEach(button => button.addEventListener('click', calculate));
 
 
 
